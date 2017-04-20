7417f8e4c1a6d20dbe39ff89185f282b13285677
Szymon Koper <sz.koper@gmail.com>
2017-04-20 15:41:58 +0200

    Removed unused imports

--- app/src/main/java/com/example/android/popularmovies/MovieDetail.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetail.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.popularmovies.data.FavoriteMovieContract;
 import com.example.android.popularmovies.data.FavoriteMovieDbUtils;
