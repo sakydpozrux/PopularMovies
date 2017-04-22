b3daf6ff991e9d9b822956d1558f29786037e890
Szymon Koper <sz.koper@gmail.com>
2017-04-22 03:45:26 +0200

    BitmapUtils for serialization

---
+++ app/src/main/java/com/example/android/popularmovies/utils/BitmapUtils.java
@@ -0,0 +1,30 @@
+package com.example.android.popularmovies.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by sakydpozrux on 22/04/2017.
+ */
+
+public class BitmapUtils {
+    private static final Bitmap.CompressFormat FORMAT = Bitmap.CompressFormat.PNG;
+    private static final int QUALITY = 100;
+    private static final int BASE64_FLAGS = Base64.DEFAULT;
+
+    public static String serialize(Bitmap poster) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        poster.compress(FORMAT, QUALITY, byteArrayOutputStream);
+        return Base64.encodeToString(byteArrayOutputStream.toByteArray(), BASE64_FLAGS);
+    }
+
+    public static Bitmap deserialize(String bitmapBase64) {
+        // TODO: Replace 1st arg with base64Str.substring(bitmapBase64.indexOf(",")  + 1) ???
+        byte[] byteArray = Base64.decode(bitmapBase64, BASE64_FLAGS);
+        Bitmap poster = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        return poster;
+    }
+}
